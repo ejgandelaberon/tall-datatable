0190522fabe9c040205893cb6dff556af89c7ae1
Emmanuel Joseph Beron <emsephron@gmail.com>
2024-10-07 23:56:47 +0800

    feat: HasTallDatatable interface

---
+++ src/HasTallDatatable.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Emsephron\TallDatatable;
+
+use Illuminate\Database\Eloquent\Builder;
+
+interface HasTallDatatable
+{
+    public function dataTable(DataTable $dataTable): DataTable;
+
+    public function fetch(array $data): array; // @phpstan-ignore-line
+
+    public function query(): Builder;
+}
